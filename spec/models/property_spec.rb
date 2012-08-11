982939709d574aa9bc2bdbcaf43b6b2e58bcf925
Percy Keeley <percy.keeley@softwareForest.com>
2012-08-11 11:33:54 -0400

    Remove some failing Model relationship tests

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe Property do
-  [:permit].each do |symbol|
+  [:permit, :permit_detail].each do |symbol|
     it "should have many #{symbol.to_s}" do
       Property.reflect_on_association(symbol).macro.should be(:has_many)
     end
   end
+  
 end
