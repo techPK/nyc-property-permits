837548b40bd96b8260391fdd10247c31071c7e35
Percy Keeley <percy.keeley@softwareForest.com>
2012-09-09 16:07:18 -0400

    Updated home and note pages with css

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@
 
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.6'
+gem 'rails', '3.2.7'
+gem 'bootstrap-sass', '~> 2.0.4.0'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- spec/requests/notices_spec.rb
+++ spec/requests/notices_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe "Notices" do
   %w(Home About Contact Terms Technology).each do |page_name|
 	  describe "#{page_name} page" do
-	    xit "should have the content '#{page_name}" do
+	    it "should have the content '#{page_name}" do
 	      visit "/notices/#{page_name.downcase}"
 	      page.should have_content("#{page_name}")
 	    end
 	    it "'#{page_name}' should have the correct title." do
 	      visit "/notices/#{page_name.downcase}"
 	      page.should have_selector('title', \
-	      			text:"NYC Property Permit Infomation | #{page_name}")
+	      			text:"NYC Property Permit Information | #{page_name}")
 	    end
 	  end
 	end
